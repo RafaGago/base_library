1f2df51b9053ca9ccf061fa96627eefa7ccbb6ad
Rafael <rafael.gago@diadrom.se>
2016-02-02 17:11:45 +0100

    backoff class

---
+++ include/bl/lib/nonblock/backoff.h
@@ -0,0 +1,35 @@
+#ifndef __BL_NONBLOCK_BACKOFF_H__
+#define __BL_NONBLOCK_BACKOFF_H__
+
+/*----------------------------------------------------------------------------*/
+#include <bl/hdr/base/integer.h>
+/*----------------------------------------------------------------------------*/
+typedef struct nonblock_backoff {
+  uword spin;
+  uword spin_max;
+  uword yield;
+  uword yield_max;
+  i32   sleep_us;
+  i32   sleep_us_mul;
+  i32   sleep_us_div;
+  i32   sleep_us_max;
+}
+nonblock_backoff;
+/*----------------------------------------------------------------------------*/
+void nonblock_backoff_init(
+  nonblock_backoff* nb, 
+  uword             spin_max,
+  uword             yield_max, 
+  i32               sleep_us_init,
+  i32               sleep_us_mul,
+  i32               sleep_us_div,
+  i32               sleep_us_max
+  );
+/*----------------------------------------------------------------------------*/
+void nonblock_backoff_run (nonblock_backoff* nb);
+/*----------------------------------------------------------------------------*/
+
+
+
+#endif /* __BL_NONBLOCK_BACKOFF_H__ */
+
