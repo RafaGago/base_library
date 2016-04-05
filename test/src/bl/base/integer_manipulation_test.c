1eacb6d7c33003dafd7e29afcb790f5aec40486f
Rafael <rafael.gago@diadrom.se>
2016-04-05 14:52:16 +0200

    fix broken integer_manipulation.h + add tests

--- test/src/bl/base/integer_manipulation_test.c
+++ test/src/bl/base/integer_manipulation_test.c
@@ -5,7 +5,6 @@
 #include <bl/base/utility.h>
 #include <bl/base/integer_manipulation.h>
 #include <bl/base/platform.h>
-
 /*---------------------------------------------------------------------------*/
 static void has_one_or_zero_bits_set_test (void** s)
 {
@@ -129,9 +128,89 @@ static void u_first_set_lsb_mask_test (void** s)
   assert_true (u64_first_set_lsb_mask (0) == 0);
 }
 /*---------------------------------------------------------------------------*/
+static void read_u64_be_test (void **s)
+{
+  u8 arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+  assert_true (read_u64_be (arr) == 0x0102030405060708);
+}
+/*---------------------------------------------------------------------------*/
+static void read_u64_le_test (void **s)
+{ 
+  u8 arr[] = {1, 2, 3, 4, 5, 6, 7, 8};  
+  assert_true (read_u64_le (arr) == 0x0807060504030201);
+}
+/*---------------------------------------------------------------------------*/
+static void write_u64_be_test (void **s)
+{
+  u8 arr[8];
+  u64 val = 0x0102030405060708;
+  write_u64_le (arr, val);
+  assert_true (arr[0] = 1);
+  assert_true (arr[1] = 2);
+  assert_true (arr[2] = 3);
+  assert_true (arr[3] = 4);
+  assert_true (arr[4] = 5);
+  assert_true (arr[5] = 6);
+  assert_true (arr[6] = 7);
+  assert_true (arr[7] = 8);
+}
+/*---------------------------------------------------------------------------*/
+static void write_u64_le_test (void **s)
+{
+  u8 arr[8];
+  u64 val = 0x0807060504030201;
+  write_u64_le (arr, val);
+  assert_true (arr[0] = 1);
+  assert_true (arr[1] = 2);
+  assert_true (arr[2] = 3);
+  assert_true (arr[3] = 4);
+  assert_true (arr[4] = 5);
+  assert_true (arr[5] = 6);
+  assert_true (arr[6] = 7);
+  assert_true (arr[7] = 8);
+}
+/*---------------------------------------------------------------------------*/
+static void read_u48_be_test (void **s)
+{
+  u8 arr[] = {1, 2, 3, 4, 5, 6};
+  assert_true (read_u48_be (arr) == 0x010203040506);
+}
+/*---------------------------------------------------------------------------*/
+static void read_u48_le_test (void **s)
+{ 
+  u8 arr[] = {1, 2, 3, 4, 5, 6};  
+  assert_true (read_u48_le (arr) == 0x060504030201);
+}
+/*---------------------------------------------------------------------------*/
+static void write_u48_be_test (void **s)
+{
+  u8 arr[6];
+  u64 val = 0x010203040506;
+  write_u48_le (arr, val);
+  assert_true (arr[0] = 1);
+  assert_true (arr[1] = 2);
+  assert_true (arr[2] = 3);
+  assert_true (arr[3] = 4);
+  assert_true (arr[4] = 5);
+  assert_true (arr[5] = 6);
+}
+/*---------------------------------------------------------------------------*/
+static void write_u48_le_test (void **s)
+{
+  u8 arr[6];
+  u64 val = 0x060504030201;
+  write_u48_le (arr, val);
+  assert_true (arr[0] = 1);
+  assert_true (arr[1] = 2);
+  assert_true (arr[2] = 3);
+  assert_true (arr[3] = 4);
+  assert_true (arr[4] = 5);
+  assert_true (arr[5] = 6);
+}
+/*---------------------------------------------------------------------------*/
 static void read_u32_be_test (void **s)
 {
-  u8 arr[] = {1, 2, 3, 4};  
+  u8 arr[] = {1, 2, 3, 4};
   assert_true (read_u32_be (arr) == 0x01020304);
 }
 /*---------------------------------------------------------------------------*/
@@ -161,6 +240,68 @@ static void write_u32_le_test (void **s)
   assert_true (arr[1] = 2);
   assert_true (arr[2] = 3);
   assert_true (arr[3] = 4);
+}
+/*---------------------------------------------------------------------------*/
+static void read_u24_be_test (void **s)
+{
+  u8 arr[] = {1, 2, 3};
+  assert_true (read_u24_be (arr) == 0x010203);
+}
+/*---------------------------------------------------------------------------*/
+static void read_u24_le_test (void **s)
+{ 
+  u8 arr[] = {1, 2, 3};  
+  assert_true (read_u24_le (arr) == 0x030201);
+}
+/*---------------------------------------------------------------------------*/
+static void write_u24_be_test (void **s)
+{
+  u8 arr[3];
+  u32 val = 0x010203;
+  write_u24_le (arr, val);
+  assert_true (arr[0] = 1);
+  assert_true (arr[1] = 2);
+  assert_true (arr[2] = 3);
+}
+/*---------------------------------------------------------------------------*/
+static void write_u24_le_test (void **s)
+{
+  u8 arr[3];
+  u32 val = 0x030201;
+  write_u24_le (arr, val);
+  assert_true (arr[0] = 1);
+  assert_true (arr[1] = 2);
+  assert_true (arr[2] = 3);
+}
+/*---------------------------------------------------------------------------*/
+static void read_u16_be_test (void **s)
+{
+  u8 arr[] = {1, 2};
+  assert_true (read_u16_be (arr) == 0x0102);
+}
+/*---------------------------------------------------------------------------*/
+static void read_u16_le_test (void **s)
+{ 
+  u8 arr[] = {1, 2};  
+  assert_true (read_u16_le (arr) == 0x0201);
+}
+/*---------------------------------------------------------------------------*/
+static void write_u16_be_test (void **s)
+{
+  u8 arr[2];
+  u16 val = 0x0102;
+  write_u16_le (arr, val);
+  assert_true (arr[0] = 1);
+  assert_true (arr[1] = 2);
+}
+/*---------------------------------------------------------------------------*/
+static void write_u16_le_test (void **s)
+{
+  u8 arr[2];
+  u16 val = 0x0201;
+  write_u16_le (arr, val);
+  assert_true (arr[0] = 1);
+  assert_true (arr[1] = 2);
 }
 /*---------------------------------------------------------------------------*/
 static void u8_get_bit_idx_test (void **s)
@@ -204,10 +345,26 @@ static const struct CMUnitTest tests[] = {
   cmocka_unit_test (u_var_set_bitv_test),
   cmocka_unit_test (u_first_clear_lsb_mask_test),
   cmocka_unit_test (u_first_set_lsb_mask_test),
+  cmocka_unit_test (read_u64_be_test),
+  cmocka_unit_test (read_u64_le_test),
+  cmocka_unit_test (write_u64_be_test),
+  cmocka_unit_test (write_u64_le_test),
+  cmocka_unit_test (read_u48_be_test),
+  cmocka_unit_test (read_u48_le_test),
+  cmocka_unit_test (write_u48_be_test),
+  cmocka_unit_test (write_u48_le_test),
   cmocka_unit_test (read_u32_be_test),
   cmocka_unit_test (read_u32_le_test),
   cmocka_unit_test (write_u32_be_test),
   cmocka_unit_test (write_u32_le_test),
+  cmocka_unit_test (read_u24_be_test),
+  cmocka_unit_test (read_u24_le_test),
+  cmocka_unit_test (write_u24_be_test),
+  cmocka_unit_test (write_u24_le_test),
+  cmocka_unit_test (read_u16_be_test),
+  cmocka_unit_test (read_u16_le_test),
+  cmocka_unit_test (write_u16_be_test),
+  cmocka_unit_test (write_u16_le_test),
   cmocka_unit_test (u8_get_bit_idx_test),
   cmocka_unit_test (u16_get_bit_idx_test),
   cmocka_unit_test (u32_get_bit_idx_test),
