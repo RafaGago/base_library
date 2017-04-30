99f092cc088b5469c9e372000de5931219039962
Rafa Gago <rafael_gago_81@hotmail.com>
2017-04-30 11:17:06 +0200

    nonblock: improve backoff

--- include/bl/nonblock/backoff.h
+++ include/bl/nonblock/backoff.h
@@ -2,6 +2,7 @@
 #define __BL_NONBLOCK_BACKOFF_H__
 
 /*----------------------------------------------------------------------------*/
+#include <bl/base/platform.h>
 #include <bl/base/integer.h>
 #include <bl/base/time.h>
 
@@ -17,29 +18,46 @@ typedef struct nonblock_backoff {
   uword   yield;
   uword   yield_max;
   toffset sleep_us;
-  toffset sleep_us_mul;
-  toffset sleep_us_div;
+  toffset sleep_mul;
+  toffset sleep_div;
   toffset sleep_us_max;
 }
 nonblock_backoff;
-/*----------------------------------------------------------------------------*/
-extern BL_NONBLOCK_EXPORT 
+/*------------------------------------------------------------------------------
+spin_max:  number of spinning loops
+yield_max: number of yield loops
+sleep: sleep uses a simple incrementing polynomial:
+
+  sleep_now = sleep_now + ((sleep_now * sleep_mul) / sleep_div)
+
+Where "sleep_ns_init" is the intitial value of "sleep_now" and "sleep_us_max"
+is the maximum value that sleep_now can take.
+
+If one wishes to have constant delays "sleep_mul" has to be set to 0.
+
+------------------------------------------------------------------------------*/
+extern BL_NONBLOCK_EXPORT
   void nonblock_backoff_init(
-    nonblock_backoff* nb, 
+    nonblock_backoff* nb,
     uword             spin_max,
-    uword             yield_max, 
+    uword             yield_max,
     toffset           sleep_us_init,
-    toffset           sleep_us_mul,
-    toffset           sleep_us_div,
+    toffset           sleep_mul,
+    toffset           sleep_div,
     toffset           sleep_us_max
     );
 /*----------------------------------------------------------------------------*/
-extern BL_NONBLOCK_EXPORT 
+extern BL_NONBLOCK_EXPORT
   void nonblock_backoff_init_default(
     nonblock_backoff* nb, toffset sleep_us_max
-    );  
+    );
+/*------------------------------------------------------------------------------
+Can be used to determine if a thread has reached the idle state
+------------------------------------------------------------------------------*/
+extern BL_NONBLOCK_EXPORT
+  toffset nonblock_backoff_next_sleep_us (nonblock_backoff* nb);
 /*----------------------------------------------------------------------------*/
-extern BL_NONBLOCK_EXPORT 
+extern BL_NONBLOCK_EXPORT
   void nonblock_backoff_run (nonblock_backoff* nb);
 /*----------------------------------------------------------------------------*/
 #ifdef __cplusplus
--- src/bl/nonblock/backoff.c
+++ src/bl/nonblock/backoff.c
@@ -6,65 +6,102 @@
 
 #include <bl/nonblock/backoff.h>
 
+#ifdef BL_LINUX
+#include <time.h>
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 /*----------------------------------------------------------------------------*/
 BL_NONBLOCK_EXPORT
 void nonblock_backoff_init(
-  nonblock_backoff* nb, 
+  nonblock_backoff* nb,
   uword             spin_max,
-  uword             yield_max, 
+  uword             yield_max,
   toffset           sleep_us_init,
-  toffset           sleep_us_mul,
-  toffset           sleep_us_div,
+  toffset           sleep_mul,
+  toffset           sleep_div,
   toffset           sleep_us_max
   )
 {
   bl_assert (sleep_us_init > 0);
-  bl_assert (sleep_us_mul >= 0);
-  bl_assert (sleep_us_div >= 0);
+  bl_assert (sleep_div >= 0);
 
   nb->spin = nb->yield = 0;
 
   nb->sleep_us     = sleep_us_init;
   nb->spin_max     = spin_max;
   nb->yield_max    = yield_max;
-  nb->sleep_us_mul = sleep_us_mul;
-  nb->sleep_us_div = sleep_us_div;
+  nb->sleep_mul    = sleep_mul;
+  nb->sleep_div    = sleep_div;
   nb->sleep_us_max = sleep_us_max;
 }
 /*----------------------------------------------------------------------------*/
-BL_NONBLOCK_EXPORT 
+BL_NONBLOCK_EXPORT
 void nonblock_backoff_init_default (nonblock_backoff* nb, toffset sleep_us_max)
 {
    nonblock_backoff_init (nb, 40, 20, BL_SCHED_TMIN_US, 1, 3, sleep_us_max);
 }
 /*----------------------------------------------------------------------------*/
-BL_NONBLOCK_EXPORT 
+static toffset nonblock_get_next_sleep (nonblock_backoff* nb)
+{
+  toffset usleep = nb->sleep_us;
+  i64 add        = nb->sleep_us * 2048;
+  add           *= nb->sleep_mul;
+  add           /= nb->sleep_div;
+  usleep        += (toffset) add / 2048;
+  usleep         = bl_max (nb->sleep_us, usleep);
+  usleep         = bl_min (nb->sleep_us_max, usleep);
+  return usleep;
+}
+/*----------------------------------------------------------------------------*/
+BL_NONBLOCK_EXPORT
+toffset nonblock_backoff_next_sleep_us (nonblock_backoff* nb)
+{
+  if (nb->spin < nb->spin_max || nb->yield < nb->yield_max) {
+    return 0;
+  }
+  return nonblock_get_next_sleep (nb);
+}
+/*----------------------------------------------------------------------------*/
+BL_NONBLOCK_EXPORT
 void nonblock_backoff_run (nonblock_backoff* nb)
 {
   if (nb->spin < nb->spin_max) {
-    ++nb->spin;
-    for (uword i = 0; i < 8; ++i) {
+    if (nb->spin < 6) {
       processor_pause();
     }
+    else if (nb->spin < 12) {
+      processor_pause();
+      processor_pause();
+    }
+    else {
+      processor_pause();
+      processor_pause();
+      processor_pause();
+      processor_pause();
+    }
+    ++nb->spin;
+    return;
   }
-  else if (nb->yield < nb->yield_max || nb->sleep_us_max < BL_SCHED_TMIN_US) {
+  if (nb->yield < nb->yield_max || nb->sleep_us_max / 1000 < BL_SCHED_TMIN_US) {
     ++nb->yield;
+#ifdef BL_LINUX
+    /*Short sleep to avoid a potential sched_yield call to return immediately*/
+    struct timespec t, rem;
+    t.tv_sec  = 0;
+    t.tv_nsec = 300;
+    (void) nanosleep (&t, &rem);
+#else
+    /*On other OS's just yield. If yielding returns immediatelly just wait for
+      the full sleep section*/
     bl_thread_yield();
+#endif
+    return;
   }
-  else {
-    bl_thread_usleep (nb->sleep_us);
-    toffset usleep = nb->sleep_us;
-    i64 add        = nb->sleep_us;
-    add           *= nb->sleep_us_mul;
-    add           /= nb->sleep_us_div;
-    usleep        += (toffset) add;
-    usleep         = bl_max (nb->sleep_us, usleep);
-    usleep         = bl_min (nb->sleep_us_max, usleep);
-    nb->sleep_us   = usleep;
-  }
+  bl_thread_usleep (nb->sleep_us);
+  nb->sleep_us = nonblock_get_next_sleep (nb);
 }
 /*----------------------------------------------------------------------------*/
 #ifdef __cplusplus
