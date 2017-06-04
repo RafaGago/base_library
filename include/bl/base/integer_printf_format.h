83be21981b70eb3b46a0606d03ff75409d2d973c
Rafa Gago <rafael_gago_81@hotmail.com>
2017-06-04 13:07:57 +0200

    base: int printf format: add length-only macros

--- include/bl/base/integer_printf_format.h
+++ include/bl/base/integer_printf_format.h
@@ -1,61 +1,63 @@
 #ifndef __BL_PRINTF_INT_H__
 #define __BL_PRINTF_INT_H__
 
-/*NOTE: this wheel was reinvented when not knowing about the format specifiers
-(e.g. PRIu64) in C99 <inttypes.h>, I kept it to avoid refactoring,. It is
-still missing some less used definitions.*/
+/*NOTE: this wheel was initially reinvented when not knowing about the format
+  specifiers (e.g. PRIu64) in C99 <inttypes.h> and then proved usefull for some
+  extra cases.
+
+This still has the advantage that it provides the FMT_Lxx macros, so
+literal concatenations can be used (were needed when developing malc). */
 
 #include <bl/base/platform.h>
 
-#define FMT_U8 "hhu"
-#define FMT_I8 "hh"
-#define FMT_X8 "hhx"
-#define FMT_O8 "hho"
+#define FMT_L8        "hh"
+#define FMT_U8 FMT_L8 "u"
+#define FMT_I8 FMT_L8 "d"
+#define FMT_X8 FMT_L8 "x"
+#define FMT_O8 FMT_L8 "o"
 
-#define FMT_U16 "hu"
-#define FMT_I16 "h"
-#define FMT_X16 "hx"
-#define FMT_O16 "ho"
+#define FMT_L16         "h"
+#define FMT_U16 FMT_L16 "u"
+#define FMT_I16 FMT_L16 "d"
+#define FMT_X16 FMT_L16 "x"
+#define FMT_O16 FMT_L16 "o"
 
 /*FMT_x32 may require fixing for 16-bit compilers, good enough for now*/
-#define FMT_U32 "u"
-#define FMT_I32 "d"
-#define FMT_X32 "x"
-#define FMT_O32 "o"
+#define FMT_L32         ""
+#define FMT_U32 FMT_L32 "u"
+#define FMT_I32 FMT_L32 "d"
+#define FMT_X32 FMT_L32 "x"
+#define FMT_O32 FMT_L32 "o"
 
 #if defined (BL_GCC) && (BL_WORDSIZE == 64)
-  #define FMT_U64 "lu"
-  #define FMT_I64 "l"
-  #define FMT_X64 "lx"
-  #define FMT_O64 "lo"
+  #define FMT_L64 "l"
 #else
   /*FMTx64 may require fixing for other compilers depending on theirs stdint.h,
     tweak as needed*/
-  #define FMT_U64 "llu"
-  #define FMT_I64 "ll"
-  #define FMT_X64 "llx"
-  #define FMT_O64 "llo"
+  #define FMT_L64 "ll"
 #endif
 
+#define FMT_U64 FMT_L64 "u"
+#define FMT_I64 FMT_L64 "d"
+#define FMT_X64 FMT_L64 "x"
+#define FMT_O64 FMT_L64 "o"
+
 #if BL_WORDSIZE == 16
-  #define FMT_UWORD FMT_U16
-  #define FMT_WORD  FMT_I16
-  #define FMT_XWORD FMT_X16
-  #define FMT_OWORD FMT_O16
+  #define FMT_LWORD FMT_L16
 #elif BL_WORDSIZE == 32
-  #define FMT_UWORD FMT_U32
-  #define FMT_WORD  FMT_I32
-  #define FMT_XWORD FMT_X32
-  #define FMT_OWORD FMT_O32
+  #define FMT_LWORD FMT_L32
 #elif BL_WORDSIZE == 64
-  #define FMT_UWORD FMT_U64
-  #define FMT_WORD  FMT_I64
-  #define FMT_XWORD FMT_X64
-  #define FMT_OWORD FMT_O64
+  #define FMT_LWORD FMT_L64
 #else
   #error "Implement this"
 #endif
 
+#define FMT_UWORD FMT_LWORD "u"
+#define FMT_WORD  FMT_LWORD "d"
+#define FMT_XWORD FMT_LWORD "x"
+#define FMT_OWORD FMT_LWORD "o"
+
+#define FMT_LW FMT_LWORD
 #define FMT_W  FMT_WORD
 #define FMT_UW FMT_UWORD
 #define FMT_XW FMT_XWORD
