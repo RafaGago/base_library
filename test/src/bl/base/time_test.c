1f2d362c023fbed1bd27b8d6dac49b99eb49b539
Rafa Gago <rafael_gago_81@hotmail.com>
2017-07-03 21:02:28 +0200

    time_test: remove warning

--- test/src/bl/base/time_test.c
+++ test/src/bl/base/time_test.c
@@ -1,4 +1,6 @@
-#define BL_TIMESTAMP_64BIT
+#ifndef BL_TIMESTAMP_64BIT
+#  define BL_TIMESTAMP_64BIT
+#endif
 
 #include <string.h>
 
@@ -9,7 +11,7 @@
 
 #include <bl/base/thread_micro_sleep.c>
 
-/*A quick check that the timestamp calculations aren't very broken by 
+/*A quick check that the timestamp calculations aren't very broken by
   sleeping the thread*/
 
 /*---------------------------------------------------------------------------*/
@@ -18,13 +20,13 @@ static void timestamp_not_very_broken (void **state)
   enum {
     sleep_us = (uword) BL_SCHED_TMIN_US * 20,
     ubound   = (uword) sleep_us * 2,
-  }; 
+  };
   tstamp start    = bl_get_tstamp();
   bl_thread_usleep (sleep_us);
   tstamp finish   = bl_get_tstamp();
   tstampdiff diff = tstamp_get_diff (finish, start);
   assert_true (diff > 0);
-  toffset us      = bl_tstamp_to_usec ((tstamp) diff);  
+  toffset us      = bl_tstamp_to_usec ((tstamp) diff);
   assert_true (us >= sleep_us && us <= ubound);
 }
 /*---------------------------------------------------------------------------*/
@@ -36,15 +38,15 @@ static void timestamp_maxes (void **state)
   max = bl_sec_to_tstamp_max();
   v   = bl_sec_to_tstamp (max);
   assert_true (v <= tstamp_max_safe_add_sub);
-  
+
   max = bl_msec_to_tstamp_max();
   v   = bl_msec_to_tstamp (max);
   assert_true (v <= tstamp_max_safe_add_sub);
-  
+
   max = bl_usec_to_tstamp_max();
   v   = bl_usec_to_tstamp (max);
   assert_true (v <= tstamp_max_safe_add_sub);
-  
+
   max = bl_nsec_to_tstamp_max();
   v   = bl_nsec_to_tstamp (max);
   assert_true (v <= tstamp_max_safe_add_sub);
