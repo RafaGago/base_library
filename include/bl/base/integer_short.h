b90a0afdf156ccc212e9546b58cab998fe06c66d
Rafa Gago <rafael_gago_81@hotmail.com>
2019-11-03 19:36:56 +0100

    integer_short: Fix wrong swapped typedefs

--- include/bl/base/integer_short.h
+++ include/bl/base/integer_short.h
@@ -7,36 +7,36 @@
 
 #include <bl/base/integer.h>
 
-typedef u8  bl_u8;
-typedef u16 bl_u16;
-typedef u32 bl_u32;
-typedef u64 bl_u64;
+typedef bl_u8  u8;
+typedef bl_u16 u16;
+typedef bl_u32 u32;
+typedef bl_u64 u64;
 
-typedef i8  bl_i8;
-typedef i16 bl_i16;
-typedef i32 bl_i32;
-typedef i64 bl_i64;
+typedef bl_i8  i8;
+typedef bl_i16 i16;
+typedef bl_i32 i32;
+typedef bl_i64 i64;
 
-typedef uword bl_uword;
-typedef word  bl_word;
+typedef bl_uword uword;
+typedef bl_word  word;
 #if BL_HAS_WORD_D2 == 1
-typedef uword_d2 bl_uword_d2;
-typedef word_d2  bl_word_d2;
+typedef bl_uword_d2 uword_d2;
+typedef bl_word_d2  word_d2;
 #endif
 #if BL_HAS_WORD_D4 == 1
-typedef uword_d4 bl_uword_d4;
-typedef word_d4  bl_word_d4;
+typedef bl_uword_d4 uword_d4;
+typedef bl_word_d4  word_d4;
 #endif
 
-typedef ubig bl_ubig;
-typedef ibig bl_ibig;
+typedef bl_ubig ubig;
+typedef bl_ibig ibig;
 #if BL_HAS_BIG_D2 == 1
-typedef ubig_d2 bl_ubig_d2;
-typedef big_d2  bl_big_d2;
+typedef bl_ubig_d2 ubig_d2;
+typedef bl_big_d2  big_d2;
 #endif
 #if BL_HAS_BIG_D4 == 1
-typedef ubig_d4 bl_ubig_d4;
-typedef big_d4  bl_big_d4;
+typedef bl_ubig_d4 ubig_d4;
+typedef bl_big_d4  big_d4;
 #endif
 
 #endif /* __BL_INTEGER_SHORT_H__ */
